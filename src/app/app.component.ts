c31c963835c1a1df12ef9423df75be1b63c48de6
Pablo <pablocastilloastudillo@gmail.com>
2021-01-14 14:01:33 -0300

    Angular 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'hola-mundo';
+  nombre = 'Pablo';
+
+  getNombre() {
+    return this.title + 'Mi nombre es: ' + this.nombre;
+  }
 }
---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cursos',
+  template: '<h1>Angular CLI</h1>',
+  styleUrls: ['./cursos.component.css'],
+})
+export class CursosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
